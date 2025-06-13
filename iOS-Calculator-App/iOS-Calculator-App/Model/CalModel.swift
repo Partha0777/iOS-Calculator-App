937ea295b8de5bc444fbd3035d19a58f3d377585
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-06-13 22:08:31 +0530

    Calculator
    Added keys name with enum

--- iOS-Calculator-App/iOS-Calculator-App/Model/CalModel.swift
+++ iOS-Calculator-App/iOS-Calculator-App/Model/CalModel.swift
@@ -2,4 +2,23 @@
 
 enum Keys: String{
     case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case zero = "0"
+    case plus = "+"
+    case minus = "-"
+    case multiply = "*"
+    case divide = "/"
+    case equal = "="
+    case clear = "AC"
+    case dot = "."
+    case plusMinus = "+/-"
+    case percentage = "%"
+
 }
